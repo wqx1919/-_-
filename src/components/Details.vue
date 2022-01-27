7a14ef05ab00ae16ebc1b176756d36c592a43fd4
wqx1919 <1229877486@qq.com>
2022-01-27 17:53:45 +0800

    备份2022年1月27日17点53分

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -31,8 +31,8 @@
       </div>
       <div class="body">
         <!-- 文章数据 -->
-        <h3 class="tltle">{{$route.params.title}}</h3>
-        <div class="text">{{$route.params.content}}</div>
+        <h3 class="tltle">{{ $route.params.title }}</h3>
+        <div class="text">{{ $route.params.content }}</div>
       </div>
       <div class="bottom">
         <i class="iconfont icon-24px"></i>
@@ -42,17 +42,16 @@
       <div class="Comment_reply">
         评论:
         <ul>
-          <li v-for="(data) in comment_results" :key="data.id">
-            id:{{ALLuersinfo(data.from_user_id)}}
-            内容:{{data.content}}
-             <Replyn :reply="handler(data.id)" />
+          <li v-for="(data,index) in comment_results" :key="index">
+            昵称:{{uersinfo[index].account}}
+            内容:{{ data.content }}
+            <Replyn :reply="handler(data.id)" />
             <!-- <div class="messages">
               回复：   {{handler(data.id)}}
             </div> -->
           </li>
         </ul>
       </div>
-      
       <el-pagination background layout="prev, pager, next" :total="100">
       </el-pagination>
     </div>
@@ -70,130 +69,193 @@ import Replyn from "./part/Replyn.vue";
 export default {
   name: "Details",
   // props:['content','title'],
-  data(){
-     return{
-       reply_results:[],
-       comment_results:[],
-       dataid:"",
-       uersinfo:[]
-     }
+  data() {
+    return {
+      reply_results: [],
+      comment_results: [],
+      dataid: "",
+      uersinfo:[]
+      // [{account:"11"},{account:"2"}],
+      // temp1:""
+    };
   },
   components: {
     Label,
     Hot,
-    Replyn
+    Replyn,
   },
   methods: {
-     getData() {
+  async   getData() {
+    // async: false;
       let _this = this;
-      let param = new URLSearchParams()
+      let param = new URLSearchParams();
       // alert(this.$route.params.id)
-      param.append("topic_id", this.$route.params.id)
+      param.append("topic_id", this.$route.params.id);
       // param.append("password", this.form.password)
-       _this.$axios.post('http://127.0.0.1:8008/api/gettopic_comment', param).then(res=>{
-         if(res.data.status===1){
-            alert(res.data.message)
-         }
-         else{
-          //  console.log(res.data)
-           _this.comment_results = res.data.data.comment_results //赋值
-           _this.reply_results = res.data.data.reply_results
-         }
-      },err=>{
-        console.log(err);
-      })
-      
-     },
-     totree(list,parId) {
-    let obj = {};
-    let result = [];
-    //将数组中数据转为键值对结构 (这里的数组和obj会相互引用)
-    list.map(el => {
-        obj[el.id] = el;
-    })
-    // let flag
-    // 头部
-	 for (const key in list) {
-             if(list[key].reply_type ==="comment"){
-                list[key].reply_id=0;
-                // flag =true
+     await _this.$axios
+        .post("http://127.0.0.1:8008/api/gettopic_comment", param)
+        .then(
+          (res) => {
+            if (res.data.status === 1) {
+              alert(res.data.message);
+            } else {
+              //  console.log(res.data)
+              _this.comment_results = res.data.data.comment_results; //赋值
+              _this.reply_results = res.data.data.reply_results;
             }
-       }
-    for(let i=0, len = list.length; i < len; i++) {
-        let id = list[i].reply_id;
-        if(id == parId) {
-            result.push(list[i]);
-            continue;
-        }
-        if(obj[id].children) {
-            obj[id].children.push(list[i]);
-        } else {
-            obj[id].children = [list[i]];
-        }
-    }
-    return result;
-    // this.replydata =result
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
     },
-     handler(id){
-        let _this  = this 
-        const list =[]
-        for(var key in this.reply_results){
-              // if(id ===_this.reply_results[key].comment_id &&  _this.reply_results[key].reply_type==="reply" ){
-              //   list.push(_this.reply_results[key])
-              // }
-              
-              if(id ===this.reply_results[key].comment_id ){
-                list.push(this.reply_results[key])
-              }
-          }
-          
-
-
-        let result = this.totree(list,0)
-        // let param2 = new URLSearchParams()
-      // alert(this.$route.params.id)
-        // param2.append("id",id)
-        for(let i =0;i<this.comment_results.length;i++){
-          if(id ===  this.comment_results[i].id)
-        _this.$axios.get('http://127.0.0.1:8008/api/alluserinfo', {params:{"id":this.comment_results[i].from_user_id}}).then(res=>{
-          if(res.data.status===1){
-              // alert(res.data.message)
-              console.log(res.data)
-          }
-          else{
-            //  console.log(res.data)
-            _this.uersinfo.push(res.data.data) //赋值
-          }
-        },err=>{
-          console.log(err);
-        })
+    totree(list, parId) {
+      let obj = {};
+      let result = [];
+      //将数组中数据转为键值对结构 (这里的数组和obj会相互引用)
+      list.map((el) => {
+        obj[el.id] = el;
+      });
+      // let flag
+      // 头部
+      for (const key in list) {
+        if (list[key].reply_type === "comment") {
+          list[key].reply_id = 0;
+          // flag =true
         }
-        return result
-     },
-     
-     ALLuersinfo(id){
-       for (const key in this.uersinfo) {
-        //  console.log(this.comment_results[key].from_user_id===id)
-         if(this.uersinfo[key].id === id){
-          //  console.log(this.uersinfo[key].account)
-           return this.uersinfo[key].account
-         }
-       }
-      //  return 
-     }
+      }
+      for (let i = 0, len = list.length; i < len; i++) {
+        let id = list[i].reply_id;
+        if (id == parId) {
+          result.push(list[i]);
+          continue;
+        }
+        if (obj[id].children) {
+          obj[id].children.push(list[i]);
+        } else {
+          obj[id].children = [list[i]];
+        }
+      }
+      return result;
+      // this.replydata =result
+    },
+    handler(id) {
+      let _this = this;
+      const list = [];
+      for (var key in this.reply_results) {
+        // if(id ===_this.reply_results[key].comment_id &&  _this.reply_results[key].reply_type==="reply" ){
+        //   list.push(_this.reply_results[key])
+        // }
 
+        if (id === this.reply_results[key].comment_id) {
+          list.push(this.reply_results[key]);
+        }
+      }
+
+      let result = this.totree(list, 0);
+      // let param2 = new URLSearchParams()
+      // alert(this.$route.params.id)
+      // param2.append("id",id)
+
+      // console.log(this.comment_results.length)
+      return result;
+    },
+
+    ALLuersinfo() {
+      // console.log(id)
+      
+      const _this =this
+      var temp1=""
+      console.log(this.comment_results)
+      for (let i = 0; i < this.comment_results.length; i++) {
+        // if (id === this.comment_results[i].from_user_id){
+          // console.log(id)
+      _this.$axios.get("http://127.0.0.1:8008/api/alluserinfo", {
+     params:{ id: this.comment_results[i].from_user_id },
+      })
+      .then(
+        (res) => {
+          if (res.data.status === 1) {
+            // alert(res.data.message)
+            console.log(res.data);
+          } else {
+             console.log(res.data.data.account)
+            _this.uersinfo.push(res.data.data); //赋值
+            // _this.temp1= res.data.data.account
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+      // console.log(temp1+"11")
+
+        // }
+      // }
+      // console.log(this.uersinfo[0].account)
+      // return this.uersinfo[0].account
+      // console.log( this.uersinfo[0].id)
+      // for(let key = 0;key<this.uersinfo.length;key++){
+      //   if (this.uersinfo[key].id === id) {
+      //     // break
+      //      console.log(this.uersinfo[key].account)
+      //     //  return this.uersinfo[key].account
+      //   }
+      // }
+      // for (let key in this.uersinfo) {
+      //   //  console.log(this.comment_results[key].from_user_id===id)
+      //   if (this.uersinfo[key].id === id) {
+      //     // break
+      //      console.log(this.uersinfo[key].account)
+      //     //  return this.uersinfo[key].account
+      //   }
+      }
+            //  return _this.temp1
+      //  return
+    },
   },
-       
-     
-  mounted() {
-     this.getData()
+
+  async  mounted() {
+   await  this.getData();
+    this.ALLuersinfo();
     //  console.log(this.$route)
   },
+  computed:{
+    //    ALLuersinfo() {
+    //   // console.log(id)       
+    //   const _this =this
+    //   var temp1=""
+    //   // console.log(this.comment_results)
+    //   for (let i = 0; i < this.comment_results.length; i++) {
+    //     // if (id === this.comment_results[i].from_user_id){
+    //       // console.log(id)
+    // _this.$axios.get("http://127.0.0.1:8008/api/alluserinfo", {
+    //     params: { id: id },
+    //   })
+    //   .then(
+    //     (res) => {
+    //       if (res.data.status === 1) {
+    //         // alert(res.data.message)
+    //         console.log(res.data);
+    //       } else {
+    //          console.log(res.data.data.account)
+    //         _this.uersinfo.push(res.data.data); //赋值
+    //         _this.temp1= res.data.data.account
+    //       }
+    //     },
+    //     (err) => {
+    //       console.log(err);
+    //     }
+    //   );
+    //   } 
+    //   return  11     //  return
+    // }
+    },
   
   //  computed:{
   //    comment_results_details(){
   //      return(id)=>{
-  //      let _this  = this 
+  //      let _this  = this
   //     //  Object.keys(_this.comment_results).forEach(function(key){
   //       //  console.log(_this.reply_results)
   //       //  console.log(key,_this.comment_results[key]);
@@ -216,15 +278,15 @@ export default {
   //     //   //  console.log(key+"11")
   //     //    return 1
   //     //     // for (const key2 in this.reply_results) {
-  //     //     //   if (key[id] == key2[comment_id]) 
+  //     //     //   if (key[id] == key2[comment_id])
   //     //     //         return 1
   //     //     //       }
   //     //     //   }
   //     //  }}
-       
+
   //  }
-   watch:{
-    //  dataid(new1,ole){   
+  watch: {
+    //  dataid(new1,ole){
     //    console.log(new1)
     //  }
     //  dataid:{
@@ -232,7 +294,7 @@ export default {
     //   //    console.log(_this.reply_results)
     //   //    console.log(key,_this.comment_results[key]);
     //     handler(oldValue,newValue){
-    //     let _this  = this 
+    //     let _this  = this
     //     console.log(_this._data.dataid)
     //     // console.log("###")
     //     console.log(oldValue+"@@"+newValue)
@@ -245,7 +307,7 @@ export default {
     //     }
     //  }
     //  comment_results_details(id){
-    //    let _this  = this 
+    //    let _this  = this
     //   //  Object.keys(_this.comment_results).forEach(function(key){
     //     //  console.log(_this.reply_results)
     //     //  console.log(key,_this.comment_results[key]);
@@ -256,7 +318,7 @@ export default {
     //          }
     //       });
     //     }
-     }
+  },
 };
 </script>
 
@@ -288,9 +350,9 @@ export default {
 /* background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%); */
 /* position: relative; */
 /* } */
-.article{
+.article {
   flex: 1;
-  position:relative;
+  position: relative;
 }
 .article .top {
   /* height: 100px; */
@@ -305,7 +367,7 @@ export default {
 font-size:14px ;
 } */
 
-.article .el-pagination{
+.article .el-pagination {
   position: absolute;
   bottom: 0;
 }
@@ -332,13 +394,12 @@ font-size:14px ;
   display: flex;
   justify-content: space-evenly;
 } */
-.tltle{
-  text-align:center;
+.tltle {
+  text-align: center;
 }
-.Comment_reply{
-
-}
-.messages{
+/* .Comment_reply {
+} */
+.messages {
   margin-left: 20px;
 }
 </style>
