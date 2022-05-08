1db62a11f43fcc25cda784acd2fcec6bc11a5ef5
wqx1919 <64262398+wqx1919@users.noreply.github.com>
2022-05-08 18:12:24 +0800

    删除无用代码
    Signed-off-by: wqx1919 <64262398+wqx1919@users.noreply.github.com>

--- router/comment.js
+++ router/comment.js
@@ -1,14 +1,4 @@
-// const expressJoi = require('@escook/express-joi')
-// const { register_schema,reg_login_schema } = require('../schema/user')
-// const user =require("../router_handler/user")
-// // const { reg_login_schema } = require('../schema/user')
-// module.exports = function(app) {
-//     app.get('/', function(req, res, next) {
-//         res.render('index', { title: 'Express' });
-//     });
-//     app.use("/ueditor", require("../router_handler/ueditor"));
-//     // app.use("/", require("./qidian"));
-// }
+
 const express = require('express')
 const router = express.Router()
 
@@ -28,7 +18,5 @@ var multer = require('multer')
 const upload = multer({ dest: './public/images/comment/' })
 router.post('/addcomentfile',upload.single('myFileName'), comment.addcomentfile)
 
-// router.post('/SELECT_topic', topic.topic)
-// console.log("1333")
 
 module.exports = router
--- router/qidian.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-router.get("/api",function(req,res,next){
-     console.log(req)
-})
-module.exports = router;   //暴露接口
