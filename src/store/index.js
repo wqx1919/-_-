9e162de49ee44e0665196c1c6d5a565d125c0b12
wqx1919 <1229877486@qq.com>
2022-05-09 14:25:06 +0800

    删除无用注释掉代码
    Signed-off-by: wqx1919 <1229877486@qq.com>

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,30 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
- 
+
 const store = new Vuex.Store({
- 
+
   state: {
     // 存储token
     Authorization: localStorage.getItem('Authorization') ? localStorage.getItem('Authorization') : '',
     // name: localStorage.getItem('name') ? localStorage.getItem('name') : '',
     user: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : '',
-    host:'http://127.0.0.1:8008'
+    host: 'http://127.0.0.1:8008'
   },
   getters: {//如果要使用watch观察状态改变那么一定配置这一项
- 
+
   },
   mutations: {
     // 修改token，并将token存入localStorage
-    changeLogin (state, user) {
+    changeLogin(state, user) {
       state.Authorization = user.Authorization;
-      // state.user = user.userinfo;
-      // localStorage.setItem('user',JSON.stringify(user.userinfo));
       localStorage.setItem('Authorization', user.Authorization);
-      // console.log( user)
-    //   alert("1")
     },
-    updateUser (state, user) {
+    updateUser(state, user) {
       state.user = user
-      localStorage.setItem('user',JSON.stringify(user));
+      localStorage.setItem('user', JSON.stringify(user));
     }
   }
 });
- 
+
 export default store;
